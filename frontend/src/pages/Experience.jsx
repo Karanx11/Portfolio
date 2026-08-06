93810f2029dedee8495065b6604a21474377725e
Karan Sharma <karanx11.72898@gmail.com>
2026-08-06 18:59:27 +0530

    Added Heading

--- frontend/src/pages/Experience.jsx
+++ frontend/src/pages/Experience.jsx
@@ -46,7 +46,7 @@ function Experience() {
     {/* LEFT */}
     <div>
       <h3 className="text-3xl font-bold text-white">
-        Automation Testing Engineer Intern
+        QA Engineer Intern
       </h3>
 
       <p className="text-[#FA7D09] text-lg font-medium mt-2">
